0694b7d166c5a7c5332ab00b671ce284a9fd0c7b
Edwin Pinchao <epinchao@krugerschool.edu.ec>
2024-10-03 11:43:44 -0500

    "Config DB mongo file"

--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import db from "./config/mongo";
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+db().then(() => console.log("Conexion Ready"));
 app.listen(PORT, () => console.log(`Listo por el puerto ${PORT}`));
